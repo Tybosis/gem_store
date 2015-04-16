0e5ff87d60dc1870f8b46eca6f042ccad77e5b8a
Tyler Pottle <tyler.pottle@gmail.com>
2015-04-15 17:39:04 -0700

    add products listing

--- test/integration/listing_products_test.rb
+++ test/integration/listing_products_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class ListingProductsTest < ActionDispatch::IntegrationTest
-  get '/products'
+  setup do
+    descrip = 'A ruby is a pink to blood-red colored gemstone, a variety of the mineral corundum'
+    Product.create!(name: 'Ruby', description: descrip, shine: 10, color: '#FF0000')
+  end
 
-  assert_equal 200, response.status
-  assert_equal Mime::JSON, response.content_type
+  test 'listing products' do
+    get '/products'
+
+    assert_equal 200, response.status
+    assert_equal Mime::JSON, response.content_type
+    assert_equal Product.count, JSON.parse(response.body).size
+  end
 end
