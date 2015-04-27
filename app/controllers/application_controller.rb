bbdd4caf7b71ab7908d069655eea392a2fae047b
Tyler Pottle <tyler.pottle@gmail.com>
2015-04-27 15:47:23 -0700

    remove commented code from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,25 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   # before_action :authenticate, except: :index
+
   def index
   end
-
-  # http_basic_authenticate_with name: 'admin', password: '12345' except: :index
-
-  # def authenticate
-  #   authenticate_token || render_unauthorized
-  # end
-
-  # def authenticate_token
-  #   authenticate_with_http_token do |token, options|
-  #     User.find_by(token: token)
-  #   end
-  # end
-
-  # def render_unauthorized
-  #   self.headers['WWW-Authenticate'] = 'Token realm="Application"'
-  #   respond_to do |format|
-  #     format.json { render json: "error", status: 401 }
-  #   end
-  # end
 end
